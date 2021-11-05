e142374d7eb2eb11891a3d2f1606776ea935a77c
RoadtoS7 <kshvv975@gmail.com>
2021-11-05 11:44:56 +0900

    Add: Database 테스트 코드

--- app/src/androidTest/java/com/smilegate/assignment/BlogDatabaseTest.kt
+++ app/src/androidTest/java/com/smilegate/assignment/BlogDatabaseTest.kt
@@ -1,10 +1,16 @@
 package com.smilegate.assignment
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.smilegate.assignment.db.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.After
 import org.junit.Assert.*
@@ -12,13 +18,43 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
-import java.util.*
+import java.util.Date
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 
 @RunWith(AndroidJUnit4::class)
 class BlogDatabaseTest {
     private lateinit var blogDao: BlogDao
     private lateinit var db: BlogDatabase
+
+    fun <T> LiveData<T>.getOrAwaitValue(
+        time: Long = 2,
+        timeUnit: TimeUnit = TimeUnit.SECONDS
+    ): T {
+        var data: T? = null
+        val latch = CountDownLatch(1)
+        val observer = object : Observer<T> {
+            override fun onChanged(o: T?) {
+                data = o
+                latch.countDown()
+                this@getOrAwaitValue.removeObserver(this)
+            }
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            this@getOrAwaitValue.observeForever(observer)
+        }
+
+
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return data as T
+    }
 
     @Before
     fun createDb() {
@@ -37,79 +73,57 @@ class BlogDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun insertAndGetPost() {
-        val testCategoryId = 1001
-        val categoryEntity = CategoryEntity(
-            id = testCategoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
-
+    fun insertAndGetPost() = runBlocking {
         val postEntity = PostEntity(
+            id = 1001,
             title = "postTitle",
             content = "postContent",
-            image = "postImage",
-            categoryId = testCategoryId,
             date = Date()
         )
 
-        blogDao.insertCategory(categoryEntity)
         blogDao.insertPost(postEntity)
 
-        val testPostEntity = blogDao.getPostsByCategory(testCategoryId)[0]
-        assertEquals(postEntity.categoryId, testPostEntity.categoryId)
+        val postEntities = blogDao.getAllPosts()
+            .getOrAwaitValue()
+            .map { it.id }
+
+        assertTrue(postEntities.size == 1)
+        assertTrue(postEntities.contains(postEntity.id))
     }
 
     @Test
     @Throws(Exception::class)
-    fun deletePost() {
-        val testCategoryId = 1001
-        val categoryEntity = CategoryEntity(
-            id = testCategoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
+    fun deletePost() = runBlocking {
         val testPostId = 1001
         val postEntity = PostEntity(
             id = testPostId,
             title = "postTitle",
             content = "postContent",
-            image = "postImage",
-            categoryId = testCategoryId,
             date = Date()
         )
 
-        blogDao.insertCategory(categoryEntity)
         blogDao.insertPost(postEntity)
         blogDao.deletePost(postEntity)
 
         val postIds = blogDao.getAllPosts()
+            .getOrAwaitValue()
             .map { it.id }
-            .toSet()
-        assertFalse(postIds.contains(testPostId))
+
+        assertFalse(postIds!!.contains(testPostId))
     }
 
     @Test
     @Throws(Exception::class)
-    fun updatePost() {
+    fun updatePost() = runBlocking {
         // given
-        val testCategoryId = 1001
-        val categoryEntity = CategoryEntity(
-            id = testCategoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
         val testPostId = 1001
         val postEntity = PostEntity(
             id = testPostId,
             title = "postTitle",
             content = "postContent",
-            image = "postImage",
-            categoryId = testCategoryId,
             date = Date()
         )
 
-        blogDao.insertCategory(categoryEntity)
         blogDao.insertPost(postEntity)
 
         val updatedContent = "updatedContent"
@@ -117,172 +131,53 @@ class BlogDatabaseTest {
             id = postEntity.id,
             title = postEntity.title,
             content = updatedContent,
-            categoryId = postEntity.categoryId,
-            image = postEntity.image,
             date = postEntity.date
         )
 
         // when
         blogDao.updatePost(contentChangedPost)
-        val afterPostEntity = blogDao.getAllPosts()[0]
+        val afterPostEntity = blogDao.getAllPosts()
+            .getOrAwaitValue()
+            .first()
 
         // then
         assertThat(afterPostEntity.content, `is`(updatedContent))
     }
 
-    @Test
-    @Throws(Exception::class)
-    fun insertAndGetCategory() {
-        val categoryEntity = CategoryEntity(
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
-
-        blogDao.insertCategory(categoryEntity)
-        val firstCategoryEntity = blogDao.getAllCategory()[0]
-        assertEquals(categoryEntity.categoryName, firstCategoryEntity.categoryName)
-    }
 
     @Test
     @Throws(Exception::class)
-    fun getPostsByCategory() {
-        val testCategoryId = 1001
-        val categoryEntity = CategoryEntity(
-            id = testCategoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
-
-        val firstPostId = 1
-        val secondPostId = 2
-        val firstPostEntity = PostEntity(
-            id = firstPostId,
-            title = "postTitle",
-            content = "postContent",
-            image = "postImage",
-            categoryId = testCategoryId,
-            date = Date()
-        )
-        val secondPostEntity = PostEntity(
-            id = secondPostId,
-            title = "postTitle",
-            content = "postContent",
-            image = "postImage",
-            categoryId = testCategoryId,
-            date = Date()
-        )
-
-        blogDao.insertCategory(categoryEntity)
-        blogDao.insertPost(firstPostEntity)
-        blogDao.insertPost(secondPostEntity)
-
-        val resultPostSet = blogDao.getPostsByCategory(testCategoryId)
-            .map { it.id }
-            .toSet()
-
-        assertTrue(resultPostSet.contains(firstPostId))
-        assertTrue(resultPostSet.contains(secondPostId))
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertAndGetAllCategory() {
-        val travelCategory = CategoryEntity(
-            parentCategoryId = null,
-            categoryName = "travel"
-        )
-        val foodCategory = CategoryEntity(
-            parentCategoryId = null,
-            categoryName = "food"
-        )
-
-        blogDao.insertCategory(travelCategory)
-        blogDao.insertCategory(foodCategory)
-
-        val categorySet = blogDao.getAllCategory()
-            .map { it.categoryName }
-            .toSet()
-
-        assertTrue(categorySet.contains(travelCategory.categoryName))
-        assertTrue(categorySet.contains(foodCategory.categoryName))
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun getChildrenCategories() {
-        // given
-        val parentCategoryId = 1001
-        val parentCategoryEntity = CategoryEntity(
-            id = parentCategoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
-
-        val firstChildCategoryEntity = CategoryEntity(
-            parentCategoryId = parentCategoryId,
-            categoryName = "categoryName"
-        )
-        val secondChildCategoryEntity = CategoryEntity(
-            parentCategoryId = parentCategoryId,
-            categoryName = "categoryName"
-        )
-
-        blogDao.insertCategory(parentCategoryEntity)
-        blogDao.insertCategory(firstChildCategoryEntity)
-        blogDao.insertCategory(secondChildCategoryEntity)
-
-        // when
-        val childrenCategories = blogDao.getChildrenCategories(parentCategoryId)
-
-        // assert
-        assertThat(childrenCategories[0].parentCategoryId, `is`(parentCategoryId))
-        assertThat(childrenCategories[1].parentCategoryId, `is`(parentCategoryId))
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertAndGetComments() {
-        // given
-        val categoryId = 1
-        val categoryEntity = CategoryEntity(
-            id = categoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
-
+    fun insertAndGetComments() = runBlocking {
         val postId = 1
         val postEntity = PostEntity(
             id = postId,
             title = "postTitle",
             content = "postContent",
-            image = "postImage",
-            categoryId = categoryId,
             date = Date()
         )
 
         val firstComment = CommentEntity(
             content = "content",
             postId = postId,
-            time = Date(),
             writer = "writer",
-            writerPw = "writerPw"
+            writerPw = "writerPw",
+            date = Date()
         )
         val secondComment = CommentEntity(
             content = "content",
             postId = postId,
-            time = Date(),
             writer = "writer",
-            writerPw = "writerPw"
+            writerPw = "writerPw",
+            date = Date()
         )
 
 
-        blogDao.insertCategory(categoryEntity)
         blogDao.insertPost(postEntity)
 
         // when
         blogDao.insertComment(firstComment)
         blogDao.insertComment(secondComment)
-        val comments = blogDao.getComments(postId)
+        val comments = blogDao.getComments(postId).value!!
 
         // then
         assertThat(comments[0].postId, `is`(postId))
@@ -291,22 +186,13 @@ class BlogDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun deleteComment() {
+    fun deleteComment() = runBlocking {
         // given
-        val categoryId = 1
-        val categoryEntity = CategoryEntity(
-            id = categoryId,
-            parentCategoryId = null,
-            categoryName = "categoryName"
-        )
-
         val postId = 1
         val postEntity = PostEntity(
             id = postId,
             title = "postTitle",
             content = "postContent",
-            image = "postImage",
-            categoryId = categoryId,
             date = Date()
         )
 
@@ -315,18 +201,18 @@ class BlogDatabaseTest {
             id = commentId,
             content = "content",
             postId = postId,
-            time = Date(),
             writer = "writer",
-            writerPw = "writerPw"
+            writerPw = "writerPw",
+            date = Date()
         )
 
-        blogDao.insertCategory(categoryEntity)
         blogDao.insertPost(postEntity)
         blogDao.insertComment(firstComment)
 
         // when
         blogDao.deleteComment(firstComment)
         val resultCommentIds = blogDao.getComments(postId)
+            .value!!
             .map { it.id }
             .toSet()
 
